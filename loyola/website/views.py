ac1b7ca6c46a30ae4d7b0578e85a999a4825dffe
mcaballero3 <mcaballero3@luc.edu>
2024-03-18 11:57:20 -0500

    basic html, first view, added some things to DB

--- loyola/website/views.py
+++ loyola/website/views.py
@@ -1,6 +1,20 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Course, Module, Lab, Submission, Assignment
 
+def education(request):
+    myCourses = Course.objects.all().values()
+    myModules = Module.objects.all().values()
+    myLabs = Lab.objects.all().values()
+    mySubmissions = Submission.objects.all().values()
+    myAssignments = Assignment.objects.all().values()
 
-def index(request):
-    return HttpResponse("Hello, world.")
+    context = {
+        'myCourses': myCourses,
+        'myModules': myModules,
+        'myLabs': myLabs,
+        'mySubmissions': mySubmissions,
+        'myAssignments': myAssignments
+    }
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render(context, request))
