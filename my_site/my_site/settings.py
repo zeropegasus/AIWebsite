50cedee32793928e38cd82d1e0162d0fe1b2fa13
zeropegasus <chiampas009@gmail.com>
2024-03-21 15:36:50 -0500

    Templates

--- my_site/my_site/settings.py
+++ my_site/my_site/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    '/Users/sophiahoman/Desktop/AIWebsite/my_site/static/css/style.css',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
