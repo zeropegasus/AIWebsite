16e4fe6421a4dc0dab1071fd1b97ee93351fdbb7
mcaballero <mcaballero3@luc.edu>
2024-03-23 13:40:55 -0500

    comments

--- loyola/website/views.py
+++ loyola/website/views.py
@@ -23,30 +23,37 @@ def program_detail_view(request, id):
     # Select program based on its id
     program = Program.objects.get(id = id)
     
+    # Template uses context to reference db entries
     context = {
         'program': program
     }
 
+    # Render program template, using context dict
     return render(request, "program.html", context)
 
 def module_detail_view(request, order):
     # Select module based on its order
     module = Module.objects.get(order = order)
     
+    # Template uses context to reference db entries
     context = {
         'module': module
     }
 
+    # Render module template, using context dict
     return render(request, "module.html", context)
 
 def workshop_detail_view(request, order):
     # Select workshop based on its order
     workshop = Workshop.objects.get(order = order)
+    # Easier to reference lab within template
     lab = workshop.this_lab
     
+    # Template uses context to reference db entries
     context = {
         'workshop': workshop,
         'lab': lab
     }
 
+    # Render workshop template, using context dict
     return render(request, "workshop.html", context)
