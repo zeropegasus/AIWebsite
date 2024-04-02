f5bb25b3ef5d3e9686825d25bbe109a3b3b95e40
mcaballero3 <mcaballero3@luc.edu>
2024-04-01 19:21:39 -0500

    CRUD program, module, workshop, lab

--- loyola/website/urls.py
+++ loyola/website/urls.py
@@ -4,8 +4,15 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', TemplateView.as_view(template_name='index.html')),
-    path('program/<int:id>/', views.program_detail_view, name='program_detail_view'),
-    path('module/<int:order>/', views.module_detail_view, name='module_detail_view'),
-    path('workshop/<int:order>/', views.workshop_detail_view, name='workshop_detail_view')
-
+    path('program/<int:id>', views.program_detail_view, name='program_detail_view'),
+    path('module/<int:order>', views.module_detail_view, name='module_detail_view'),
+    path('workshop/<int:order>', views.workshop_detail_view, name='program_detail_view'),
+    path('program/create', views.program_create_view, name='module_create_view'),
+    path('program/<int:id>/update_delete', views.program_update_delete_view, name='program_update_delete_view'),
+    path('module/create', views.module_create_view, name='module_create_view'),
+    path('module/<int:order>/update_delete', views.module_update_delete_view, name='module_update_delete_view'),
+    path('workshop/create', views.workshop_create_view, name='workshop_create_view'),
+    path('workshop/<int:order>/update_delete', views.workshop_update_delete_view, name='workshop_update_delete_view'),
+    path('lab/create', views.lab_create_view, name='lab_create_view'),
+    path('lab/<int:order>/update_delete', views.lab_update_delete_view, name='lab_update_delete_view'),
 ]
--- loyola/website/views.py
+++ loyola/website/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import HttpResponse, render
+from django.shortcuts import HttpResponseRedirect, render, get_object_or_404
 from django.template import loader
 from .models import Program, Module, Workshop, Lab, Assignment, Submission, Instructor, Student
+
+from .forms import ProgramsForm, ModulesForm, WorkshopsForm, LabsForm
 
 def education(request):
     myPrograms = Program.objects.all().values()
@@ -31,6 +33,48 @@ def program_detail_view(request, id):
     # Render program template, using context dict
     return render(request, "program.html", context)
 
+def program_create_view(request):
+    context = {}
+
+    form = ProgramsForm(request.POST or None, request.FILES or None)
+
+    # check if form data is valid
+    if form.is_valid():
+        # Save form data to model
+        form.save()
+        return HttpResponseRedirect("/program/1")
+    
+    context['form'] = form
+    return render(request, "crud/program_create.html", context)
+
+def program_update_delete_view(request, id):
+    program = Program.objects.get(id = id)
+    context = {
+        'program': program
+    }
+    obj = get_object_or_404(Program, id=id)
+
+    if request.method == "POST":
+        # Check if the form was submitted for update
+        if 'update' in request.POST:
+            form = ProgramsForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect("/program/1")
+
+        # Check if the form was submitted for delete
+        if 'delete' in request.POST:
+            obj.delete()
+            return HttpResponseRedirect("/program/1")
+
+    else:
+        form = ProgramsForm(instance=obj)
+
+    context["form"] = form
+    context["id"] = id
+
+    return render(request, "crud/program_update_delete.html", context)
+
 def module_detail_view(request, order):
     # Select module based on its order
     module = Module.objects.get(order = order)
@@ -42,6 +86,49 @@ def module_detail_view(request, order):
 
     # Render module template, using context dict
     return render(request, "module.html", context)
+
+def module_create_view(request):
+    context = {}
+
+    form = ModulesForm(request.POST or None, request.FILES or None)
+
+    # check if form data is valid
+    if form.is_valid():
+        # Save form data to model
+        form.save()
+        return HttpResponseRedirect("/program/1")
+    
+    context['form'] = form
+    return render(request, "crud/module_create.html", context)
+
+def module_update_delete_view(request, order):
+    module = Module.objects.get(order = order)
+    context = {
+        'module': module
+    }
+    obj = get_object_or_404(Module, order=order)
+
+    if request.method == "POST":
+        # Check if the form was submitted for update
+        if 'update' in request.POST:
+            form = ModulesForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect("/program/1")
+
+        # Check if the form was submitted for delete
+        if 'delete' in request.POST:
+            obj.delete()
+            return HttpResponseRedirect("/program/1")
+
+    else:
+        form = ModulesForm(instance=obj)
+
+    context["form"] = form
+    context["order"] = order
+
+    return render(request, "crud/module_update_delete.html", context)
+
 
 def workshop_detail_view(request, order):
     # Select workshop based on its order
@@ -57,3 +144,85 @@ def workshop_detail_view(request, order):
 
     # Render workshop template, using context dict
     return render(request, "workshop.html", context)
+
+def workshop_create_view(request):
+    context = {}
+
+    form = WorkshopsForm(request.POST or None, request.FILES or None)
+
+    # check if form data is valid
+    if form.is_valid():
+        # Save form data to model
+        form.save()
+        return HttpResponseRedirect("/program/1")
+    
+    context['form'] = form
+    return render(request, "crud/workshop_create.html", context)
+
+def workshop_update_delete_view(request, order):
+    workshop = Workshop.objects.get(order = order)
+    context = {
+        'workshop': workshop
+    }
+    obj = get_object_or_404(Workshop, order=order)
+
+    if request.method == "POST":
+        # Check if the form was submitted for update
+        if 'update' in request.POST:
+            form = WorkshopsForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect("/program/1")
+
+        # Check if the form was submitted for delete
+        if 'delete' in request.POST:
+            obj.delete()
+            return HttpResponseRedirect("/program/1")
+
+    else:
+        form = WorkshopsForm(instance=obj)
+
+    context["form"] = form
+    context["order"] = order
+
+    return render(request, "crud/workshop_update_delete.html", context)                                          
+
+def lab_create_view(request):
+    context = {}
+
+    form = LabsForm(request.POST or None, request.FILES or None)
+
+    # check if form data is valid
+    if form.is_valid():
+        # Save form data to model
+        form.save()
+        return HttpResponseRedirect("/program/1")
+    
+    context['form'] = form
+    return render(request, "crud/lab_create.html", context)
+
+def lab_update_delete_view(request, order):
+    context = {}
+
+    obj = get_object_or_404(Lab, order=order)
+
+    if request.method == "POST":
+        # Check if the form was submitted for update
+        if 'update' in request.POST:
+            form = LabsForm(request.POST, instance=obj)
+            if form.is_valid():
+                form.save()
+                return HttpResponseRedirect("/program/1")
+
+        # Check if the form was submitted for delete
+        if 'delete' in request.POST:
+            obj.delete()
+            return HttpResponseRedirect("/program/1")
+
+    else:
+        form = LabsForm(instance=obj)
+
+    context["form"] = form
+    context["order"] = order
+
+    return render(request, "crud/lab_update_delete.html", context)  
